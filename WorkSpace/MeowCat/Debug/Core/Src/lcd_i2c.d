a9cf9235fe00677bb5c4ac37779548bc92bf7e54
Aaron Escoboza Villegas <aaron.villegas@hotmail.com>
2021-05-30 14:34:40 -0700

    Initial commit and add lcd 16x2 driver

---
+++ WorkSpace/MeowCat/Debug/Core/Src/lcd_i2c.d
@@ -0,0 +1,85 @@
+Core/Src/lcd_i2c.o: ../Core/Src/lcd_i2c.c \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Core/Inc/lcd_i2c.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Core/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Include/cmsis_version.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Core/Inc/lcd_i2c.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Core/Inc/stm32f4xx_hal_conf.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Include/cmsis_version.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Include/mpu_armv7.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Users/aaron/Desktop/git_MeowCat/WorkSpace/MeowCat/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
