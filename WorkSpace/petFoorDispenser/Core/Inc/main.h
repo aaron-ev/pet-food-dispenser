7218b807a3fcff72a986de275c15516bfb995e11
Aaron Escoboza Villegas <aaron.villegas@hotmail.com>
2021-06-14 23:28:04 -0700

    add programmable time

--- WorkSpace/petFoorDispenser/Core/Inc/main.h
+++ WorkSpace/petFoorDispenser/Core/Inc/main.h
@@ -19,12 +19,13 @@ typedef enum
 #define SOURCE_NOTHING		  50
 
 #define ROW_BACK   	 0
-#define ROW_SPEED  	 1
-#define ROW_CYCLES 	 2
-#define ROW_FEED 	 3
-#define ROW_SETTING  4
-#define ROW_SERVING  5
-#define ROW_CENTER	 6
+#define ROW_TIME	 1
+#define ROW_SPEED  	 2
+#define ROW_CYCLES 	 3
+#define ROW_FEED 	 4
+#define ROW_SETTING  5
+#define ROW_SERVING  6
+#define ROW_CENTER	 7
 
 //#define BUTTON_DISPENSE_PIN 	GPIO_PIN_1
 #define BUTTON_ENTER_PIN 		GPIO_PIN_0
@@ -63,9 +64,9 @@ int settings[2] = {10,41};
 arrow_t arrow = {{10,10},{10,30},{0,10},{10,21},{10,41}};
 */
 
-int arrow[7][2] = {{33,40},{33,20},{33,0},{33,21},{33,41},{10,40},{20,20}}; // back,speed,cycles,feed,setting,serving,center
-int hourxy[5][2] = {{20,0},{43,0},{55,0},{78,0},{90,0}};
-
+int arrow[8][2] = {{33,20},{33,40},{33,20},{33,0},{33,21},{33,41},{10,40},{20,20}}; // back,time,speed,cycles,feed,setting,serving,center
+int hourxy[3][2] = {{20,0},{55,0},{90,0}};//hour:minutes:seconds
+int hourDot[2][2] = {{43,0},{78,0}};
 int	arrow_row = ROW_FEED;
 
 int itSource = SOURCE_NOTHING;
@@ -98,10 +99,30 @@ void tim7_Init(void);
 // error handler functions
 void Error_Handler(void);
 // RTC functions
-void set_time(void);
+void set_time(RTC_TimeTypeDef *time);
 
 // enable and disable buttons
 void enable_it_buttons(void);
 void disable_it_buttons(void);
 
+/*
+//  @modulations
+#define QPSK      0
+#define QAM_16    1
+#define QAM_64    2
+
+typedef enum
+{
+    busy,
+    done,
+}status;
+
+/*	function:
+    input: @modulations
+/*
+void detector_writeConfReg(uint32_t mod);
+void detector_writeMemIn(char *str);
+uint32_t detector_readMemOut(void);
+status detector_readStatus(void);
+*/
 #endif
